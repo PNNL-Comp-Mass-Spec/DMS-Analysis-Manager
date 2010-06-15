53e263b5e520330679e11408760c935d13d40575
John Sandoval <john.sandoval@pnnl.gov>
2010-06-15 20:00:47 +0000

    Added support for pipeline aggregate jobs.  This is the original code for the PRIDE MzXml submission plug-in.

---
+++ AM_PRIDE_MzXML_PlugIn/clsAnalysisResourcesPRIDEMzXML.vb
@@ -0,0 +1,55 @@
+Option Strict On
+
+Imports AnalysisManagerBase
+Imports System.Collections.Generic
+Imports System.IO
+
+Public Class clsAnalysisResourcesPRIDEMzXML
+    Inherits clsAnalysisResources
+
+    Public Overrides Function GetResources() As AnalysisManagerBase.IJobParams.CloseOutType
+
+        'Clear out list of files to delete or keep when packaging the results
+        clsGlobal.ResetFilesToDeleteOrKeep()
+
+        Dim SplitString As String()
+        Dim FileNameExt As String()
+
+        SplitString = m_jobParams.GetParam("TargetJobFileList").Split(","c)
+        For Each row As String In SplitString
+            FileNameExt = row.Split(":"c)
+            If FileNameExt(2) = "nocopy" Then
+                clsGlobal.m_FilesToDeleteExt.Add(FileNameExt(1))
+            End If
+        Next
+
+        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Getting PRIDE MzXML Input file")
+
+        If Not RetrieveFile(m_jobParams.GetParam("PRIDEMzXMLInputFile"), _
+         m_jobParams.GetParam("transferFolderPath"), _
+         m_mgrParams.GetParam("workdir")) _
+        Then Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+
+        clsGlobal.FilesToDelete.Add(m_jobParams.GetParam("PRIDEMzXMLInputFile"))
+
+        If Not RetrieveAggregateFiles(SplitString) Then
+            'Errors were reported in function call, so just return
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        Dim ext As String
+        Dim DumFiles() As String
+
+        'update list of files to be deleted after run
+        For Each ext In clsGlobal.m_FilesToDeleteExt
+            DumFiles = Directory.GetFiles(m_mgrParams.GetParam("workdir"), "*" & ext) 'Zipped DTA
+            For Each FileToDel As String In DumFiles
+                clsGlobal.FilesToDelete.Add(FileToDel)
+            Next
+        Next
+
+        Return IJobParams.CloseOutType.CLOSEOUT_SUCCESS
+
+    End Function
+
+End Class
---
+++ AM_PRIDE_MzXML_PlugIn/clsAnalysisToolRunnerPRIDEMzXML.vb
@@ -0,0 +1,170 @@
+Option Strict On
+
+'*********************************************************************************************************
+' Written by John Sandoval for the US Department of Energy 
+' Pacific Northwest National Laboratory, Richland, WA
+' Copyright 2010, Battelle Memorial Institute
+'
+'*********************************************************************************************************
+
+imports AnalysisManagerBase
+Imports PRISM.Files
+Imports AnalysisManagerBase.clsGlobal
+
+Public Class clsAnalysisToolRunnerPRIDEMzXML
+    Inherits clsAnalysisToolRunnerBase
+
+    '*********************************************************************************************************
+    'Class for running PRIDEMzXML analysis
+    '*********************************************************************************************************
+
+#Region "Module Variables"
+    Protected Const PROGRESS_PCT_PRIDEMZXML_RUNNING As Single = 5
+    Protected Const PROGRESS_PCT_START As Single = 95
+    Protected Const PROGRESS_PCT_COMPLETE As Single = 99
+
+    Protected WithEvents CmdRunner As clsRunDosProgram
+    '--------------------------------------------------------------------------------------------
+    'Future section to monitor PRIDE_MzXml log file for progress determination
+    '--------------------------------------------------------------------------------------------
+    'Dim WithEvents m_StatFileWatch As FileSystemWatcher
+    'Protected m_XtSetupFile As String = "default_input.xml"
+    '--------------------------------------------------------------------------------------------
+    'End future section
+    '--------------------------------------------------------------------------------------------
+#End Region
+
+#Region "Methods"
+    ''' <summary>
+    ''' Runs DTA_Refinery tool
+    ''' </summary>
+    ''' <returns>CloseOutType enum indicating success or failure</returns>
+    ''' <remarks></remarks>
+    Public Overrides Function RunTool() As IJobParams.CloseOutType
+
+        Dim result As IJobParams.CloseOutType
+
+        'Do the base class stuff
+        If Not MyBase.RunTool = IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+
+        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Running MSDataFileTrimmer")
+
+        CmdRunner = New clsRunDosProgram(m_WorkDir)
+
+        If m_DebugLevel > 4 Then
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, "clsAnalysisToolRunnerPRIDEMzXML.RunTool(): Enter")
+        End If
+
+        ' verify that program file exists
+        ' DTARefineryLoc will be something like this: "C:\DMS_Programs\MSDataFileTrimmer\MSDataFileTrimmer.exe"
+        Dim progLoc As String = m_mgrParams.GetParam("MSDataFileTrimmerprogloc")
+        If Not System.IO.File.Exists(progLoc) Then
+            If progLoc.Length = 0 Then progLoc = "Parameter 'MSDataFileTrimmerprogloc' not defined for this manager"
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "Cannot find MSDataFileTrimmer program file: " & progLoc)
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        Dim CmdStr As String
+        CmdStr = "/M:" & System.IO.Path.Combine(m_WorkDir, m_jobParams.GetParam("PRIDEMzXMLInputFile"))
+        CmdStr &= " /G /O:" & m_WorkDir
+
+        If Not CmdRunner.RunProgram(progLoc, CmdStr, "MSDataFileTrimmer", True) Then
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.ERROR, "Error running MSDataFileTrimmer, job " & m_JobNum)
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        'Stop the job timer
+        m_StopTime = System.DateTime.Now
+
+        'Add the current job data to the summary file
+        If Not UpdateSummaryFile() Then
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.WARN, "Error creating summary file, job " & m_JobNum & ", step " & m_jobParams.GetParam("Step"))
+        End If
+
+        'Make sure objects are released
+        System.Threading.Thread.Sleep(2000)        '2 second delay
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+
+        result = MakeResultsFolder()
+        If result <> IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+            'TODO: What do we do here?
+            Return result
+        End If
+
+        Dim DumFiles() As String
+
+        'update list of files to be deleted after run
+        DumFiles = System.IO.Directory.GetFiles(m_mgrParams.GetParam("workdir"), "*_grouped*") 'Zipped DTA
+        For Each FileToSave As String In DumFiles
+            clsGlobal.m_ExceptionFiles.Add(System.IO.Path.GetFileName(FileToSave))
+        Next
+
+        result = MoveResultFiles()
+        If result <> IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+            'TODO: What do we do here?
+            Return result
+        End If
+
+        result = CopyResultsFolderToServer()
+        If result <> IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+            'TODO: What do we do here?
+            Return result
+        End If
+
+        If Not clsGlobal.RemoveNonResultFiles(m_mgrParams.GetParam("workdir"), m_DebugLevel) Then
+            'TODO: Figure out what to do here
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        Return IJobParams.CloseOutType.CLOSEOUT_SUCCESS 'ZipResult
+
+    End Function
+
+    ''' <summary>
+    ''' Event handler for CmdRunner.LoopWaiting event
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub CmdRunner_LoopWaiting() Handles CmdRunner.LoopWaiting
+        Static dtLastStatusUpdate As System.DateTime = System.DateTime.Now
+
+        ' Synchronize the stored Debug level with the value stored in the database
+        Const MGR_SETTINGS_UPDATE_INTERVAL_SECONDS As Integer = 300
+        MyBase.GetCurrentMgrSettingsFromDB(MGR_SETTINGS_UPDATE_INTERVAL_SECONDS)
+
+        'Update the status file (limit the updates to every 5 seconds)
+        If System.DateTime.Now.Subtract(dtLastStatusUpdate).TotalSeconds >= 5 Then
+            dtLastStatusUpdate = System.DateTime.Now
+            m_StatusTools.UpdateAndWrite(IStatusFile.EnumMgrStatus.RUNNING, IStatusFile.EnumTaskStatus.RUNNING, IStatusFile.EnumTaskStatusDetail.RUNNING_TOOL, PROGRESS_PCT_PRIDEMZXML_RUNNING, 0, "", "", "", False)
+        End If
+
+    End Sub
+
+    '--------------------------------------------------------------------------------------------
+    'Future section to monitor log file for progress determination
+    '--------------------------------------------------------------------------------------------
+    '	Private Sub StartFileWatcher(ByVal DirToWatch As String, ByVal FileToWatch As String)
+
+    ''Watches the DTA_Refinery status file and reports changes
+
+    ''Setup
+    'm_StatFileWatch = New FileSystemWatcher
+    'With m_StatFileWatch
+    '	.BeginInit()
+    '	.Path = DirToWatch
+    '	.IncludeSubdirectories = False
+    '	.Filter = FileToWatch
+    '	.NotifyFilter = NotifyFilters.LastWrite Or NotifyFilters.Size
+    '	.EndInit()
+    'End With
+
+    ''Start monitoring
+    'm_StatFileWatch.EnableRaisingEvents = True
+
+    '	End Sub
+    '--------------------------------------------------------------------------------------------
+    'End future section
+    '--------------------------------------------------------------------------------------------
+#End Region
+
+End Class
