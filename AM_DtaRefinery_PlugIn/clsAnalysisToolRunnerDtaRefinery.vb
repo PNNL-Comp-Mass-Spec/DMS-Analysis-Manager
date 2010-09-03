0f41af71f88925415d4a6ac483c3a843d8cd5f35
Matthew Monroe <matthew.monroe@pnnl.gov>
2010-09-03 01:40:54 +0000

    Fixed benign MoveResultFiles logic bug

--- AM_DtaRefinery_PlugIn/clsAnalysisToolRunnerDtaRefinery.vb
+++ AM_DtaRefinery_PlugIn/clsAnalysisToolRunnerDtaRefinery.vb
@@ -165,7 +165,8 @@ Public Class clsAnalysisToolRunnerDtaRefinery
         result = MakeResultsFolder()
         If result = IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
             ' Move the result files into the result folder
-            If result = MoveResultFiles() Then
+            result = MoveResultFiles()
+            If result = IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
                 ' Move was a success; update strFolderPathToArchive
                 strFolderPathToArchive = System.IO.Path.Combine(m_WorkDir, m_ResFolderName)
             End If
