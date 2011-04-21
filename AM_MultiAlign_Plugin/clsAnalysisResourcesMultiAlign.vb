9f5b9f9df787050c84137e1a8a00efca8c5706c2
John Sandoval <john.sandoval@pnnl.gov>
2011-04-21 23:45:56 +0000

    This plugin supports running MultiAlign in the DMS pipeline.

---
+++ AM_MultiAlign_Plugin/clsAnalysisResourcesMultiAlign.vb
@@ -0,0 +1,113 @@
+Option Strict On
+
+'*********************************************************************************************************
+' Written by Matthew Monroe for the US Department of Energy 
+' Pacific Northwest National Laboratory, Richland, WA
+' Copyright 2009, Battelle Memorial Institute
+'
+'*********************************************************************************************************
+
+Imports AnalysisManagerBase
+
+Public Class clsAnalysisResourcesMultiAlign
+    Inherits clsAnalysisResources
+
+    Public Const FEATURES_FILE_SUFFIX As String = "_LCMSFeatures.txt"
+
+    Public Overrides Function GetResources() As AnalysisManagerBase.IJobParams.CloseOutType
+
+        Dim result As Boolean
+        Dim strFileToGet As String
+        Dim strMAParamFileName As String
+        Dim strMAParameterFileStoragePath As String
+        Dim strParamFileStoragePathKeyName As String
+
+        'Clear out list of files to delete or keep when packaging the results
+        clsGlobal.ResetFilesToDeleteOrKeep()
+
+        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Getting required files")
+
+        ' Retrieve FeatureFinder _LCMSFeatures.txt file for this dataset
+        strFileToGet = m_jobParams.GetParam("DatasetNum") & FEATURES_FILE_SUFFIX
+        If Not FindAndRetrieveMiscFiles(strFileToGet, False) Then
+            'Errors were reported in function call, so just return
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        ' Retrieve the MultiAlign Parameter .xml file specified for this job
+        strMAParamFileName = m_jobParams.GetParam("ParmFileName")
+        If strMAParamFileName Is Nothing OrElse strMAParamFileName.Length = 0 Then
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "MultiAlign ParmFileName not defined in the settings for this job; unable to continue")
+            Return IJobParams.CloseOutType.CLOSEOUT_NO_PARAM_FILE
+        End If
+
+        strParamFileStoragePathKeyName = AnalysisManagerBase.clsAnalysisMgrSettings.STEPTOOL_PARAMFILESTORAGEPATH_PREFIX & "MultiAlign"
+        strMAParameterFileStoragePath = m_mgrParams.GetParam(strParamFileStoragePathKeyName)
+        If strMAParameterFileStoragePath Is Nothing OrElse strMAParameterFileStoragePath.Length = 0 Then
+            strMAParameterFileStoragePath = "\\gigasax\DMS_Parameter_Files\MultiAlign"
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN, "Parameter '" & strParamFileStoragePathKeyName & "' is not defined (obtained using V_Pipeline_Step_Tools_Detail_Report in the Broker DB); will assume: " & strMAParameterFileStoragePath)
+        End If
+
+        If Not CopyFileToWorkDir(strMAParamFileName, strMAParameterFileStoragePath, m_WorkingDir) Then
+            'Errors were reported in function call, so just return
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        ' Build the MultiAlign input text file
+        result = BuildMultiAlignInputTextFile()
+
+        If Not result Then
+            Dim Msg As String = "clsAnalysisResourcesMultiAlign.GetResources(), failed building MultiAlign input.txt file "
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, Msg)
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        Return IJobParams.CloseOutType.CLOSEOUT_SUCCESS
+
+    End Function
+
+    Protected Function BuildMultiAlignInputTextFile() As Boolean
+
+        Const INPUT_FILENAME As String = "files.txt"
+
+        Dim result As Boolean = True
+        Dim swOutFile As System.IO.StreamWriter
+
+        Dim TargetFilePath As String = System.IO.Path.Combine(m_WorkingDir, INPUT_FILENAME)
+        Dim DatasetFilePath As String = System.IO.Path.Combine(m_WorkingDir, m_jobParams.GetParam("DatasetNum") & FEATURES_FILE_SUFFIX)
+
+        Dim blnInputFileDefined As Boolean
+        Dim blnOutputDirectoryDefined As Boolean
+
+        blnInputFileDefined = False
+        blnOutputDirectoryDefined = False
+        result = True
+
+        ' Create the MA input file 
+        Try
+
+            swOutFile = New System.IO.StreamWriter(New System.IO.FileStream(TargetFilePath, IO.FileMode.Create, IO.FileAccess.Write, IO.FileShare.Read))
+
+            swOutFile.WriteLine("[Files]")
+            swOutFile.WriteLine(DatasetFilePath)
+            '..\SARC_MS_Final\663878_Sarc_MS_13_24Aug10_Cheetah_10-08-02_0000_LCMSFeatures.txt
+
+            swOutFile.WriteLine("[Database]")
+
+            swOutFile.WriteLine("Database = " & m_jobParams.GetParam("AMTDB"))
+            swOutFile.WriteLine("Server = " & m_jobParams.GetParam("AMTDBServer"))
+            'Database = MT_Human_Sarcopenia_MixedLC_P692
+            'Server = elmer
+
+            swOutFile.Close()
+
+        Catch ex As Exception
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "clsAnalysisResourcesMultiAlign.BuildMultiAlignInputTextFile, Error buliding the input .txt file (" & INPUT_FILENAME & "): " & ex.Message)
+            result = False
+
+        End Try
+
+        Return result
+    End Function
+
+End Class
---
+++ AM_MultiAlign_Plugin/clsAnalysisToolRunnerMultiAlign.vb
@@ -0,0 +1,206 @@
+Option Strict On
+
+'*********************************************************************************************************
+' Written by Matthew Monroe for the US Department of Energy 
+' Pacific Northwest National Laboratory, Richland, WA
+' Copyright 2009, Battelle Memorial Institute
+'
+'*********************************************************************************************************
+
+Imports AnalysisManagerBase
+'Imports PRISM.Files
+'Imports AnalysisManagerBase.clsGlobal
+
+Public Class clsAnalysisToolRunnerMultiAlign
+    Inherits clsAnalysisToolRunnerBase
+
+    '*********************************************************************************************************
+    ' Class for running the MultiAlign
+    '*********************************************************************************************************
+
+#Region "Module Variables"
+    Protected Const PROGRESS_PCT_MULTI_ALIGN_RUNNING As Single = 5
+    Protected Const PROGRESS_PCT_MULTI_ALIGN_DONE As Single = 95
+
+    Protected WithEvents CmdRunner As clsRunDosProgram
+
+#End Region
+
+#Region "Methods"
+    ''' <summary>
+    ''' Runs MultiAlign tool
+    ''' </summary>
+    ''' <returns>CloseOutType enum indicating success or failure</returns>
+    ''' <remarks></remarks>
+    Public Overrides Function RunTool() As IJobParams.CloseOutType
+
+        Dim CmdStr As String
+        Dim result As IJobParams.CloseOutType
+        Dim blnSuccess As Boolean
+
+        'Do the base class stuff
+        If Not MyBase.RunTool = IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+
+        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Running MultiAlign")
+
+        CmdRunner = New clsRunDosProgram(m_WorkDir)
+
+        If m_DebugLevel > 4 Then
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, "clsAnalysisToolRunnerMultiAlign.OperateAnalysisTool(): Enter")
+        End If
+
+        ' verify that program file exists
+        Dim progLoc As String = m_mgrParams.GetParam("MultiAlignProgLoc")
+        If Not System.IO.File.Exists(progLoc) Then
+            If progLoc.Length = 0 Then
+                m_message = "Manager parameter MultiAlignProgLoc is not defined in the Manager Control DB"
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_message)
+            Else
+                m_message = "Cannot find MultiAlign program file"
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_message & ": " & progLoc)
+            End If
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        Dim MultiAlignDatabaseName As String = " " & m_jobParams.GetParam("DatasetNum") & ".db3"
+
+        ' Set up and execute a program runner to run MultiAlign
+        CmdStr = " files.txt " & System.IO.Path.Combine(m_WorkDir, m_jobParams.GetParam("ParmFileName")) & " " & m_WorkDir & MultiAlignDatabaseName
+        If m_DebugLevel >= 1 Then
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, progLoc & " " & CmdStr)
+        End If
+
+        With CmdRunner
+            .CreateNoWindow = True
+            .CacheStandardOutput = True
+            .EchoOutputToConsole = True
+
+            .WriteConsoleOutputToFile = False
+        End With
+
+        If Not CmdRunner.RunProgram(progLoc, CmdStr, "MultiAlignProgLoc", True) Then
+            m_message = "Error running MultiAlign"
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_message & ", job " & m_JobNum)
+            blnSuccess = False
+        Else
+            blnSuccess = True
+        End If
+
+        'Stop the job timer
+        m_StopTime = System.DateTime.Now
+        m_progress = PROGRESS_PCT_MULTI_ALIGN_DONE
+
+        'Add the current job data to the summary file
+        If Not UpdateSummaryFile() Then
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN, "Error creating summary file, job " & m_JobNum & ", step " & m_jobParams.GetParam("Step"))
+        End If
+
+        'Make sure objects are released
+        System.Threading.Thread.Sleep(2000)        '2 second delay
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+
+        If Not blnSuccess Then
+            ' Move the source files and any results to the Failed Job folder
+            ' Useful for debugging MultiAlign problems
+            CopyFailedResultsToArchiveFolder()
+            Return IJobParams.CloseOutType.CLOSEOUT_FAILED
+        End If
+
+        result = MakeResultsFolder()
+        If result <> IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+            'TODO: What do we do here?
+            Return result
+        End If
+
+        result = MoveResultFiles()
+        If result <> IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+            'TODO: What do we do here?
+            ' Note that MoveResultFiles should have already called clsAnalysisResults.CopyFailedResultsToArchiveFolder
+            Return result
+        End If
+
+        ' Move the Plots folder to the result files folder
+        Dim diPlotsFolder As System.IO.DirectoryInfo
+        diPlotsFolder = New System.IO.DirectoryInfo(System.IO.Path.Combine(m_WorkDir, "Plots"))
+
+        Dim strTargetFolderPath As String
+        strTargetFolderPath = System.IO.Path.Combine(System.IO.Path.Combine(m_WorkDir, m_ResFolderName), "Plots")
+        diPlotsFolder.MoveTo(strTargetFolderPath)
+
+        result = CopyResultsFolderToServer()
+        If result <> IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+            'TODO: What do we do here?
+            ' Note that CopyResultsFolderToServer should have already called clsAnalysisResults.CopyFailedResultsToArchiveFolder
+            Return result
+        End If
+
+        Return IJobParams.CloseOutType.CLOSEOUT_SUCCESS 'ZipResult
+
+    End Function
+
+    Protected Sub CopyFailedResultsToArchiveFolder()
+
+        Dim result As IJobParams.CloseOutType
+
+        Dim strFailedResultsFolderPath As String = m_mgrParams.GetParam("FailedResultsFolderPath")
+        If String.IsNullOrEmpty(strFailedResultsFolderPath) Then strFailedResultsFolderPath = "??Not Defined??"
+
+        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN, "Processing interrupted; copying results to archive folder: " & strFailedResultsFolderPath)
+
+        ' Bump up the debug level if less than 2
+        If m_DebugLevel < 2 Then m_DebugLevel = 2
+
+        ' Try to save whatever files are in the work directory (however, delete the .UIMF file first, plus also the Decon2LS .csv files)
+        Dim strFolderPathToArchive As String
+        strFolderPathToArchive = String.Copy(m_WorkDir)
+
+        Try
+            System.IO.File.Delete(System.IO.Path.Combine(m_WorkDir, m_Dataset & ".UIMF"))
+            System.IO.File.Delete(System.IO.Path.Combine(m_WorkDir, m_Dataset & "*.csv"))
+        Catch ex As Exception
+            ' Ignore errors here
+        End Try
+
+        ' Make the results folder
+        result = MakeResultsFolder()
+        If result = IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+            ' Move the result files into the result folder
+            result = MoveResultFiles()
+            If result = IJobParams.CloseOutType.CLOSEOUT_SUCCESS Then
+                ' Move was a success; update strFolderPathToArchive
+                strFolderPathToArchive = System.IO.Path.Combine(m_WorkDir, m_ResFolderName)
+            End If
+        End If
+
+        ' Copy the results folder to the Archive folder
+        Dim objAnalysisResults As clsAnalysisResults = New clsAnalysisResults(m_mgrParams, m_jobParams)
+        objAnalysisResults.CopyFailedResultsToArchiveFolder(strFolderPathToArchive)
+
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Event handler for CmdRunner.LoopWaiting event
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub CmdRunner_LoopWaiting() Handles CmdRunner.LoopWaiting
+        Static dtLastStatusUpdate As System.DateTime = System.DateTime.Now
+
+        ' Synchronize the stored Debug level with the value stored in the database
+        Const MGR_SETTINGS_UPDATE_INTERVAL_SECONDS As Integer = 300
+        MyBase.GetCurrentMgrSettingsFromDB(MGR_SETTINGS_UPDATE_INTERVAL_SECONDS)
+
+        'Update the status file (limit the updates to every 5 seconds)
+        If System.DateTime.Now.Subtract(dtLastStatusUpdate).TotalSeconds >= 5 Then
+            dtLastStatusUpdate = System.DateTime.Now
+            m_StatusTools.UpdateAndWrite(IStatusFile.EnumMgrStatus.RUNNING, IStatusFile.EnumTaskStatus.RUNNING, IStatusFile.EnumTaskStatusDetail.RUNNING_TOOL, PROGRESS_PCT_MULTI_ALIGN_RUNNING, 0, "", "", "", False)
+        End If
+
+    End Sub
+
+
+#End Region
+
+End Class
