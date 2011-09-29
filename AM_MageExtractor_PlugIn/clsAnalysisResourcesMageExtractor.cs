bd0a418d3a96d502dbb12215511d1541f7346c24
Gary Kiebel <Gary.Kiebel@pnnl.gov>
2011-09-29 19:02:07 +0000

    Initial commit

---
+++ AM_MageExtractor_PlugIn/clsAnalysisResourcesMageExtractor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AnalysisManagerBase;
+using PRISM;
+using Mage;
+
+namespace AnalysisManager_MageExtractor_PlugIn
+{
+
+    public class clsAnalysisResourcesMageExtractor : clsAnalysisResources
+    {
+
+
+        public override AnalysisManagerBase.IJobParams.CloseOutType GetResources()
+        {
+              return IJobParams.CloseOutType.CLOSEOUT_SUCCESS;
+        }
+ 
+    }
+}
