8197ae909f6cd20f9230ef361b94e6af8c2e03e4
John Sandoval <john.sandoval@pnnl.gov>
2011-12-02 16:58:42 +0000

    This is a test project for testing the Ape.dll and plug-in code.

---
+++ TestApePlugIn/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TestApePlugIn {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Test_GetImprovResults = new System.Windows.Forms.Button();
+            this.Test_RunWorkflow = new System.Windows.Forms.Button();
+            this.Test_Tool_Runner = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Test_GetImprovResults
+            // 
+            this.Test_GetImprovResults.Location = new System.Drawing.Point(16, 12);
+            this.Test_GetImprovResults.Name = "Test_GetImprovResults";
+            this.Test_GetImprovResults.Size = new System.Drawing.Size(200, 23);
+            this.Test_GetImprovResults.TabIndex = 0;
+            this.Test_GetImprovResults.Text = "Test GetImproveResults";
+            this.Test_GetImprovResults.UseVisualStyleBackColor = true;
+            this.Test_GetImprovResults.Click += new System.EventHandler(this.Test_GetImprovResults_Click);
+            // 
+            // Test_RunWorkflow
+            // 
+            this.Test_RunWorkflow.Location = new System.Drawing.Point(16, 41);
+            this.Test_RunWorkflow.Name = "Test_RunWorkflow";
+            this.Test_RunWorkflow.Size = new System.Drawing.Size(200, 23);
+            this.Test_RunWorkflow.TabIndex = 1;
+            this.Test_RunWorkflow.Text = "Test RunWorkflow";
+            this.Test_RunWorkflow.UseVisualStyleBackColor = true;
+            this.Test_RunWorkflow.Click += new System.EventHandler(this.Test_RunWorkflow_Click);
+            // 
+            // Test_Tool_Runner
+            // 
+            this.Test_Tool_Runner.Location = new System.Drawing.Point(12, 265);
+            this.Test_Tool_Runner.Name = "Test_Tool_Runner";
+            this.Test_Tool_Runner.Size = new System.Drawing.Size(219, 23);
+            this.Test_Tool_Runner.TabIndex = 2;
+            this.Test_Tool_Runner.Text = "Test Tool Runner";
+            this.Test_Tool_Runner.UseVisualStyleBackColor = true;
+            this.Test_Tool_Runner.Click += new System.EventHandler(this.Test_Tool_Runner_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 300);
+            this.Controls.Add(this.Test_Tool_Runner);
+            this.Controls.Add(this.Test_RunWorkflow);
+            this.Controls.Add(this.Test_GetImprovResults);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Test_GetImprovResults;
+        private System.Windows.Forms.Button Test_RunWorkflow;
+        private System.Windows.Forms.Button Test_Tool_Runner;
+    }
+}
+
---
+++ TestApePlugIn/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Mage;
+using AnalysisManager_Ape_PlugIn;
+
+namespace TestApePlugIn {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Test_Tool_Runner_Click(object sender, EventArgs e) {
+            TestToolRunnerApe ttr = new TestToolRunnerApe();
+            ttr.TestRunWorkflow();
+        }
+
+        private void Test_RunWorkflow_Click(object sender, EventArgs e)
+        {
+            TestAMApeOperations tpp = new TestAMApeOperations();
+            tpp.Test_RunWorkflow();
+        }
+
+        private void Test_GetImprovResults_Click(object sender, EventArgs e)
+        {
+            TestAMApeOperations tpp = new TestAMApeOperations();
+            tpp.Test_GetImprovResults();
+        }
+
+
+    }
+}
