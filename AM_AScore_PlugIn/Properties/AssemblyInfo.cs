96655aef0e25f9d2b9b9763567fe2bbf1b7fedc8
John Sandoval <john.sandoval@pnnl.gov>
2011-12-22 22:44:00 +0000

    Updated project and added solution.

--- AM_AScore_PlugIn/Properties/AssemblyInfo.cs
+++ AM_AScore_PlugIn/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AM_Ape_PlugIn")]
+[assembly: AssemblyTitle("AM_AScore_PlugIn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AM_Ape_PlugIn")]
+[assembly: AssemblyProduct("AM_AScore_PlugIn")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
