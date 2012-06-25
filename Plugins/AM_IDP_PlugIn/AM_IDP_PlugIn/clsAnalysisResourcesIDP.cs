690e9ca44e6670af70ed4135f7a674f786b486f5
Joseph Brown <Joseph.Brown@pnnl.gov>
2012-06-25 20:51:18 +0000

    Aggregate IDPicker Program (IDP)

---
+++ Plugins/AM_IDP_PlugIn/AM_IDP_PlugIn/clsAnalysisResourcesIDP.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using AnalysisManagerBase;
+using System.IO;
+
+namespace AnalysisManager_IDP_PlugIn
+{
+    public class clsAnalysisResourcesIDP : clsAnalysisResources
+    {
+        public static string AppFilePath = "";
+
+        public override AnalysisManagerBase.IJobParams.CloseOutType GetResources()
+        {
+
+            try
+            {
+
+                if (m_DebugLevel >= 1)
+                {
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Retrieving input files");
+                }
+                
+                string dataPackageFolderPath = Path.Combine(m_jobParams.GetParam("transferFolderPath"), m_jobParams.GetParam("OutputFolderName"));
+                string analysisType = m_jobParams.GetParam("AnalysisType");
+
+                if (!CopyFileToWorkDir("Results.db3", Path.Combine(dataPackageFolderPath, m_jobParams.GetParam("StepInputFolderName")), m_WorkingDir))
+                {
+                    m_message = "Results.db3 file from Step2 failed to copy over to working directory";
+                    //Errors were reported in function call, so just return
+                    return IJobParams.CloseOutType.CLOSEOUT_FAILED;
+                }
+
+                string strInputFileExtension = string.Empty;
+
+                // Retrieve the Cyclops Workflow file specified for this job
+                string strIDPsWorkflowFileName = m_jobParams.GetParam("CyclopsWorkflowName");
+
+                // Retrieve the Workflow file name specified for this job
+                if (string.IsNullOrEmpty(strIDPsWorkflowFileName))
+                {
+                    m_message = "IDP Workflow not defined in the job parameters for this job";
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_message + "; unable to continue");
+                    return IJobParams.CloseOutType.CLOSEOUT_NO_PARAM_FILE;
+                }
+
+                System.IO.DirectoryInfo diRemoteIDPScriptFolder;
+
+                string strIDPsWorkflowFileStoragePath = "\\\\gigasax\\DMS_Workflows\\IDP\\" + analysisType;
+                diRemoteIDPScriptFolder = new System.IO.DirectoryInfo(strIDPsWorkflowFileStoragePath);
+                
+                //Now copy the IDP workflow file to the working directory
+                if (!CopyFileToWorkDir(strIDPsWorkflowFileStoragePath, diRemoteIDPScriptFolder.FullName, m_WorkingDir))
+                {
+                    //Errors were reported in function call, so just return
+                    return IJobParams.CloseOutType.CLOSEOUT_FAILED;
+                }
+            }
+            catch (Exception ex)
+            {
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "Exception retrieving resources", ex);
+            }
+
+            return IJobParams.CloseOutType.CLOSEOUT_SUCCESS;
+        }
+    }
+}
---
+++ Plugins/AM_IDP_PlugIn/AM_IDP_PlugIn/clsAnalysisToolRunnerIDP.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using AnalysisManagerBase;
+using IDP;
+using log4net;
+
+namespace AnalysisManager_IDP_PlugIn
+{
+    public class clsAnalysisToolRunnerIDP : clsAnalysisToolRunnerBase
+    {
+        protected const float PROGRESS_PCT_IDP_START = 5;
+        protected const float PROGRESS_PCT_IDP_DONE = 95;
+
+        public override IJobParams.CloseOutType RunTool()
+        {
+            try
+            {
+
+                IJobParams.CloseOutType result = default(IJobParams.CloseOutType);
+                bool blnSuccess = false;
+
+                //Do the base class stuff
+                if (!(base.RunTool() == IJobParams.CloseOutType.CLOSEOUT_SUCCESS))
+                {
+                    return IJobParams.CloseOutType.CLOSEOUT_FAILED;
+                }
+
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Running IDP");
+                m_progress = PROGRESS_PCT_IDP_START;
+                m_StatusTools.UpdateAndWrite(IStatusFile.EnumMgrStatus.RUNNING, IStatusFile.EnumTaskStatus.RUNNING, IStatusFile.EnumTaskStatusDetail.RUNNING_TOOL, m_progress);
+
+
+                if (m_DebugLevel > 4)
+                {
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, "clsAnalysisToolRunnerIDP.RunTool(): Enter");
+                }
+
+                // Store the Cyclops version info in the database
+                if (!StoreToolVersionInfo())
+                {
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "Aborting since StoreToolVersionInfo returned false");
+                    m_message = "Error determining IDP version";
+                    return IJobParams.CloseOutType.CLOSEOUT_FAILED;
+                }
+                
+
+                Dictionary<string, string> d_Params = new Dictionary<string, string>();
+                d_Params.Add("Job", m_jobParams.GetParam("Job"));
+                d_Params.Add("IDPWorkflowName", m_jobParams.GetParam("IDPWorkflowName"));
+                d_Params.Add("workDir", m_WorkDir);
+
+                //Change the name of the log file for the local log file to the plug in log filename
+                String LogFileName = Path.Combine(m_WorkDir, "IDP_Log");
+                log4net.GlobalContext.Properties["LogName"] = LogFileName;
+                clsLogTools.ChangeLogFileName(LogFileName);
+
+                try
+                {
+                    clsIDP idp = new clsIDP(d_Params);
+                    blnSuccess = idp.Run();
+
+                    //Change the name of the log file for the local log file to the plug in log filename
+                    LogFileName = m_mgrParams.GetParam("logfilename");
+                    log4net.GlobalContext.Properties["LogName"] = LogFileName;
+                    clsLogTools.ChangeLogFileName(LogFileName);
+                }
+                catch (Exception ex)
+                {
+                    //Change the name of the log file for the local log file to the plug in log filename
+                    LogFileName = m_mgrParams.GetParam("logfilename");
+                    log4net.GlobalContext.Properties["LogName"] = LogFileName;
+                    clsLogTools.ChangeLogFileName(LogFileName);
+
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "Error running IDP: " + ex.Message);
+                    blnSuccess = false;
+                }
+
+                //Stop the job timer
+                m_StopTime = System.DateTime.UtcNow;
+                m_progress = PROGRESS_PCT_IDP_DONE;
+
+                //Add the current job data to the summary file
+                if (!UpdateSummaryFile())
+                {
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN, "Error creating summary file, job " + m_JobNum + ", step " + m_jobParams.GetParam("Step"));
+                }
+
+                //Make sure objects are released
+                //2 second delay
+                System.Threading.Thread.Sleep(2000);
+                PRISM.Processes.clsProgRunner.GarbageCollectNow();
+
+                if (!blnSuccess)
+                {
+                    // Move the source files and any results to the Failed Job folder
+                    // Useful for debugging MultiAlign problems
+                    CopyFailedResultsToArchiveFolder();
+                    return IJobParams.CloseOutType.CLOSEOUT_FAILED;
+                }
+
+                m_ResFolderName = m_jobParams.GetParam("StepOutputFolderName");
+                m_Dataset = m_jobParams.GetParam("OutputFolderName");
+                m_jobParams.SetParam("StepParameters", "OutputFolderName", m_ResFolderName);
+
+                result = MakeResultsFolder();
+                if (result != IJobParams.CloseOutType.CLOSEOUT_SUCCESS)
+                {
+                    // MakeResultsFolder handles posting to local log, so set database error message and exit
+                    return result;
+                }
+
+                result = MoveResultFiles();
+                if (result != IJobParams.CloseOutType.CLOSEOUT_SUCCESS)
+                {
+                    // Note that MoveResultFiles should have already called clsAnalysisResults.CopyFailedResultsToArchiveFolder
+                    return result;
+                }
+
+                // Move the Plots folder to the result files folder
+                System.IO.DirectoryInfo diPlotsFolder = default(System.IO.DirectoryInfo);
+                diPlotsFolder = new System.IO.DirectoryInfo(System.IO.Path.Combine(m_WorkDir, "IDPickerResults"));
+
+                if (diPlotsFolder.Exists)
+                {
+                    string strTargetFolderPath = System.IO.Path.Combine(System.IO.Path.Combine(m_WorkDir, m_ResFolderName), "IDPickerResults");
+                    diPlotsFolder.MoveTo(strTargetFolderPath);
+                }
+
+                result = CopyResultsFolderToServer();
+                if (result != IJobParams.CloseOutType.CLOSEOUT_SUCCESS)
+                {
+                    // Note that CopyResultsFolderToServer should have already called clsAnalysisResults.CopyFailedResultsToArchiveFolder
+                    return result;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                m_message = "Error in IDPsPlugin->RunTool";
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_message, ex);
+                return IJobParams.CloseOutType.CLOSEOUT_FAILED;
+            }
+
+            return IJobParams.CloseOutType.CLOSEOUT_SUCCESS;
+        }
+
+        protected void CopyFailedResultsToArchiveFolder()
+        {
+            IJobParams.CloseOutType result = default(IJobParams.CloseOutType);
+
+            string strFailedResultsFolderPath = m_mgrParams.GetParam("FailedResultsFolderPath");
+            if (string.IsNullOrEmpty(strFailedResultsFolderPath))
+                strFailedResultsFolderPath = "??Not Defined??";
+
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN, "Processing interrupted; copying results to archive folder: " + strFailedResultsFolderPath);
+
+            // Bump up the debug level if less than 2
+            if (m_DebugLevel < 2)
+                m_DebugLevel = 2;
+
+            // Try to save whatever files are in the work directory
+            string strFolderPathToArchive;
+            strFolderPathToArchive = string.Copy(m_WorkDir);
+
+            // If necessary, delete extra files with the following
+            /* 
+                try
+                {
+                    System.IO.File.Delete(System.IO.Path.Combine(m_WorkDir, m_Dataset + ".UIMF"));
+                    System.IO.File.Delete(System.IO.Path.Combine(m_WorkDir, m_Dataset + "*.csv"));
+                }
+                catch
+                {
+                    // Ignore errors here
+                }
+            */
+
+            // Make the results folder
+            result = MakeResultsFolder();
+            if (result == IJobParams.CloseOutType.CLOSEOUT_SUCCESS)
+            {
+                // Move the result files into the result folder
+                result = MoveResultFiles();
+                if (result == IJobParams.CloseOutType.CLOSEOUT_SUCCESS)
+                {
+                    // Move was a success; update strFolderPathToArchive
+                    strFolderPathToArchive = System.IO.Path.Combine(m_WorkDir, m_ResFolderName);
+                }
+            }
+
+            // Copy the results folder to the Archive folder
+            clsAnalysisResults objAnalysisResults = new clsAnalysisResults(m_mgrParams, m_jobParams);
+            objAnalysisResults.CopyFailedResultsToArchiveFolder(strFolderPathToArchive);
+
+        }
+
+
+        /// <summary>
+        /// Stores the tool version info in the database
+        /// </summary>
+        /// <remarks></remarks>
+        protected bool StoreToolVersionInfo()
+        {
+
+            string strToolVersionInfo = string.Empty;
+
+            if (m_DebugLevel >= 2)
+            {
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, "Determining tool version info");
+            }
+
+            try
+            {
+                System.Reflection.AssemblyName oAssemblyName = System.Reflection.Assembly.Load("IDP").GetName();
+
+                string strNameAndVersion;
+                strNameAndVersion = oAssemblyName.Name + ", Version=" + oAssemblyName.Version.ToString();
+                strToolVersionInfo = clsGlobal.AppendToComment(strToolVersionInfo, strNameAndVersion);
+            }
+            catch (Exception ex)
+            {
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "Exception determining Assembly info for IDP: " + ex.Message);
+                return false;
+            }
+
+            // Store paths to key DLLs
+            System.Collections.Generic.List<System.IO.FileInfo> ioToolFiles = new System.Collections.Generic.List<System.IO.FileInfo>();
+            ioToolFiles.Add(new System.IO.FileInfo("IDP.dll"));
+
+            try
+            {
+                return base.SetStepTaskToolVersion(strToolVersionInfo, ioToolFiles);
+            }
+            catch (Exception ex)
+            {
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "Exception calling SetStepTaskToolVersion: " + ex.Message);
+                return false;
+            }
+
+
+        }
+    }
+}
