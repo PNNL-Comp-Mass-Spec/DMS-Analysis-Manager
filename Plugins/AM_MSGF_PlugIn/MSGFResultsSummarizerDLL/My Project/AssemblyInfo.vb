07643d3e6c3f98e1b9b9821928f1a820e6062970
alchemistmatt <matthew.monroe@pnnl.gov>
2016-09-29 15:17:04 -0700

    Refactoring code into MSGFResultsSummarizerDLL

---
+++ Plugins/AM_MSGF_PlugIn/MSGFResultsSummarizerDLL/My Project/AssemblyInfo.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MSGFResultsSummarizer")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("PNNL")> 
+<Assembly: AssemblyProduct("MSGFResultsSummarizer")> 
+<Assembly: AssemblyCopyright("Copyright © PNNL 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("10874972-786a-4328-aeef-8d252f9cb499")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.*")> 
