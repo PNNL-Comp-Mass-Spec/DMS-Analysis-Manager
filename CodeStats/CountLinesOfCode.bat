fe33c16bed7188c62a0b29958462f166f67cd40c
alchemistmatt <matthew.monroe@pnnl.gov>
2017-01-26 17:20:13 -0800

    Move code stats files

--- CodeStats/CountLinesOfCode.bat
+++ CodeStats/CountLinesOfCode.bat
@@ -15,4 +15,3 @@ echo Stats for AM_Shared >> CodeStats.txt
 C:\Strawberry\perl\bin\perl.exe ..\..\CountLinesOfCode\cloc-1.72.pl AM_Program\AM_Shared --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused --quiet >> CodeStats.txt
 
 for /D %%i in (Plugins\*) do echo %%i && echo. >> CodeStats.txt && echo Stats for %%i >> CodeStats.txt && C:\Strawberry\perl\bin\perl.exe ..\..\CountLinesOfCode\cloc-1.72.pl %%i  --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused,MSGFResultsSummarizerExe --quiet >> CodeStats.txt
-
