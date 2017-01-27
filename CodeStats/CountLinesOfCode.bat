1ce3185799778c0873fdc921c6f033cb81e1d69b
alchemistmatt <matthew.monroe@pnnl.gov>
2017-01-26 17:27:06 -0800

    Update script to fix paths and remove stats from the output file

--- CodeStats/CountLinesOfCode.bat
+++ CodeStats/CountLinesOfCode.bat
@@ -8,10 +8,18 @@ echo.
 
 echo. >> CodeStats.txt
 echo Stats for AM_Program >> CodeStats.txt
-C:\Strawberry\perl\bin\perl.exe ..\..\CountLinesOfCode\cloc-1.72.pl AM_Program --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused,AM_Shared --quiet >> CodeStats.txt
+C:\Strawberry\perl\bin\perl.exe ..\..\..\CountLinesOfCode\cloc-1.72.pl ..\AM_Program --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused,AM_Shared --quiet >> CodeStats.txt
 
 echo. >> CodeStats.txt
 echo Stats for AM_Shared >> CodeStats.txt
-C:\Strawberry\perl\bin\perl.exe ..\..\CountLinesOfCode\cloc-1.72.pl AM_Program\AM_Shared --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused --quiet >> CodeStats.txt
+C:\Strawberry\perl\bin\perl.exe ..\..\..\CountLinesOfCode\cloc-1.72.pl ..\AM_Program\AM_Shared --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused --quiet >> CodeStats.txt
 
-for /D %%i in (Plugins\*) do echo %%i && echo. >> CodeStats.txt && echo Stats for %%i >> CodeStats.txt && C:\Strawberry\perl\bin\perl.exe ..\..\CountLinesOfCode\cloc-1.72.pl %%i  --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused,MSGFResultsSummarizerExe --quiet >> CodeStats.txt
+for /D %%i in (..\Plugins\*) do echo %%i && echo. >> CodeStats.txt && echo Stats for %%i >> CodeStats.txt && C:\Strawberry\perl\bin\perl.exe ..\..\..\CountLinesOfCode\cloc-1.72.pl %%i  --exclude-ext=XML,vbproj,csproj,bat --exclude-dir=obj,_Trash,_Unused,MSGFResultsSummarizerExe --quiet >> CodeStats.txt
+
+type CodeStats.txt  | sed -r "s/..\\//" > CodeStats2.txt
+type CodeStats2.txt | sed -r "s/(github\.com\/AlDanial\/cloc v *[0-9.]+).+/\1/" > CodeStats3.txt
+
+del CodeStats.txt
+del CodeStats2.txt
+
+move CodeStats3.txt CodeStats.txt
