b285d8cc98496963d03c3c494500dc72fdb001f9
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2017-02-13 15:14:40 -0800

    AM_MSAlign_Quant_Plugin: code conversion from VB.Net to C#

--- Plugins/AM_MSAlign_Quant_Plugin/clsAnalysisResourcesMSAlignQuant.cs
+++ Plugins/AM_MSAlign_Quant_Plugin/clsAnalysisResourcesMSAlignQuant.cs
@@ -1,99 +1,118 @@
-Option Strict On
+using System.IO;
+using AnalysisManagerBase;
 
-Imports AnalysisManagerBase
-Imports System.IO
+namespace AnalysisManagerMSAlignQuantPlugIn
+{
+    public class clsAnalysisResourcesMSAlignQuant : clsAnalysisResources
+    {
+        public const string MSALIGN_RESULT_TABLE_SUFFIX = "_MSAlign_ResultTable.txt";
 
-Public Class clsAnalysisResourcesMSAlignQuant
-    Inherits clsAnalysisResources
+        public override CloseOutType GetResources()
+        {
+            // Retrieve shared resources, including the JobParameters file from the previous job step
+            var result = GetSharedResources();
+            if (result != CloseOutType.CLOSEOUT_SUCCESS)
+            {
+                return result;
+            }
 
-    Public Const MSALIGN_RESULT_TABLE_SUFFIX As String = "_MSAlign_ResultTable.txt"
+            // Retrieve the MSAlign_Quant parameter file
+            // For example, MSAlign_Quant_Workflow_2012-07-25
 
-    Public Overrides Function GetResources() As CloseOutType
+            var strParamFileStoragePathKeyName = clsGlobal.STEPTOOL_PARAMFILESTORAGEPATH_PREFIX + "MSAlign_Quant";
+            var strParamFileStoragePath = m_mgrParams.GetParam(strParamFileStoragePathKeyName);
+            if (strParamFileStoragePath == null || strParamFileStoragePath.Length == 0)
+            {
+                strParamFileStoragePath = "\\\\gigasax\\DMS_Parameter_Files\\DeconToolsWorkflows";
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN,
+                    "Parameter '" + strParamFileStoragePathKeyName +
+                    "' is not defined (obtained using V_Pipeline_Step_Tools_Detail_Report in the Broker DB); will assume: " + strParamFileStoragePath);
+            }
 
-        ' Retrieve shared resources, including the JobParameters file from the previous job step
-        Dim result = GetSharedResources()
-        If result <> CloseOutType.CLOSEOUT_SUCCESS Then
-            Return result
-        End If
+            string strParamFileName = m_jobParams.GetJobParameter("MSAlignQuantParamFile", string.Empty);
+            if (string.IsNullOrEmpty(strParamFileName))
+            {
+                m_message = clsAnalysisToolRunnerBase.NotifyMissingParameter(m_jobParams, "MSAlignQuantParamFile");
+                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_message);
+                return CloseOutType.CLOSEOUT_NO_PARAM_FILE;
+            }
 
-        ' Retrieve the MSAlign_Quant parameter file
-        ' For example, MSAlign_Quant_Workflow_2012-07-25
+            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Getting data files");
 
-        Dim strParamFileStoragePathKeyName = clsGlobal.STEPTOOL_PARAMFILESTORAGEPATH_PREFIX & "MSAlign_Quant"
-        Dim strParamFileStoragePath = m_mgrParams.GetParam(strParamFileStoragePathKeyName)
-        If strParamFileStoragePath Is Nothing OrElse strParamFileStoragePath.Length = 0 Then
-            strParamFileStoragePath = "\\gigasax\DMS_Parameter_Files\DeconToolsWorkflows"
-            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN, "Parameter '" & strParamFileStoragePathKeyName & "' is not defined (obtained using V_Pipeline_Step_Tools_Detail_Report in the Broker DB); will assume: " & strParamFileStoragePath)
-        End If
+            if (!FileSearch.RetrieveFile(strParamFileName, strParamFileStoragePath))
+            {
+                return CloseOutType.CLOSEOUT_NO_PARAM_FILE;
+            }
 
-        Dim strParamFileName As String = m_jobParams.GetJobParameter("MSAlignQuantParamFile", String.Empty)
-        If String.IsNullOrEmpty(strParamFileName) Then
-            m_message = clsAnalysisToolRunnerBase.NotifyMissingParameter(m_jobParams, "MSAlignQuantParamFile")
-            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_message)
-            Return CloseOutType.CLOSEOUT_NO_PARAM_FILE
-        End If
+            // Retrieve the MSAlign results for this job
+            string strMSAlignResultsTable = null;
+            strMSAlignResultsTable = DatasetName + MSALIGN_RESULT_TABLE_SUFFIX;
+            if (!FileSearch.FindAndRetrieveMiscFiles(strMSAlignResultsTable, false))
+            {
+                //Errors were reported in function call, so just return
+                return CloseOutType.CLOSEOUT_FILE_NOT_FOUND;
+            }
+            m_jobParams.AddResultFileToSkip(strMSAlignResultsTable);
 
-        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.INFO, "Getting data files")
+            // Get the instrument data file
+            string strRawDataType = m_jobParams.GetParam("RawDataType");
 
-        If Not FileSearch.RetrieveFile(strParamFileName, strParamFileStoragePath) Then
-            Return CloseOutType.CLOSEOUT_NO_PARAM_FILE
-        End If
+            switch (strRawDataType.ToLower())
+            {
+                case RAW_DATA_TYPE_DOT_RAW_FILES:
+                case RAW_DATA_TYPE_BRUKER_FT_FOLDER:
+                case RAW_DATA_TYPE_DOT_D_FOLDERS:
+                    if (FileSearch.RetrieveSpectra(strRawDataType))
+                    {
+                        if (!base.ProcessMyEMSLDownloadQueue(m_WorkingDir, MyEMSLReader.Downloader.DownloadFolderLayout.FlatNoSubfolders))
+                        {
+                            return CloseOutType.CLOSEOUT_FAILED;
+                        }
 
-        ' Retrieve the MSAlign results for this job
-        Dim strMSAlignResultsTable As String
-        strMSAlignResultsTable = DatasetName & MSALIGN_RESULT_TABLE_SUFFIX
-        If Not FileSearch.FindAndRetrieveMiscFiles(strMSAlignResultsTable, False) Then
-            'Errors were reported in function call, so just return
-            Return CloseOutType.CLOSEOUT_FILE_NOT_FOUND
-        End If
-        m_jobParams.AddResultFileToSkip(strMSAlignResultsTable)
+                        // Confirm that the .Raw or .D folder was actually copied locally
+                        if (strRawDataType.ToLower() == RAW_DATA_TYPE_DOT_RAW_FILES)
+                        {
+                            if (!File.Exists(Path.Combine(m_WorkingDir, DatasetName + DOT_RAW_EXTENSION)))
+                            {
+                                m_message = "Thermo .Raw file not successfully copied to WorkDir; likely a timeout error";
+                                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR,
+                                    "clsDtaGenResources.GetResources: " + m_message);
+                                return CloseOutType.CLOSEOUT_FAILED;
+                            }
+                            m_jobParams.AddResultFileExtensionToSkip(DOT_RAW_EXTENSION);  //Raw file
+                        }
+                        else if (strRawDataType.ToLower() == RAW_DATA_TYPE_BRUKER_FT_FOLDER)
+                        {
+                            if (!Directory.Exists(Path.Combine(m_WorkingDir, DatasetName + DOT_D_EXTENSION)))
+                            {
+                                m_message = "Bruker .D folder not successfully copied to WorkDir; likely a timeout error";
+                                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR,
+                                    "clsDtaGenResources.GetResources: " + m_message);
+                                return CloseOutType.CLOSEOUT_FAILED;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG,
+                            "clsDtaGenResources.GetResources: Error occurred retrieving spectra.");
+                        return CloseOutType.CLOSEOUT_FAILED;
+                    }
+                    break;
+                default:
+                    m_message = "Dataset type " + strRawDataType + " is not supported";
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR,
+                        "clsDtaGenResources.GetResources: " + m_message + "; must be " + RAW_DATA_TYPE_DOT_RAW_FILES + " or " +
+                        RAW_DATA_TYPE_BRUKER_FT_FOLDER);
+                    return CloseOutType.CLOSEOUT_FAILED;
+            }
 
+            if (!base.ProcessMyEMSLDownloadQueue(m_WorkingDir, MyEMSLReader.Downloader.DownloadFolderLayout.FlatNoSubfolders))
+            {
+                return CloseOutType.CLOSEOUT_FAILED;
+            }
 
-        ' Get the instrument data file
-        Dim strRawDataType As String = m_jobParams.GetParam("RawDataType")
-
-        Select Case strRawDataType.ToLower
-            Case RAW_DATA_TYPE_DOT_RAW_FILES, RAW_DATA_TYPE_BRUKER_FT_FOLDER, RAW_DATA_TYPE_DOT_D_FOLDERS
-                If FileSearch.RetrieveSpectra(strRawDataType) Then
-
-                    If Not MyBase.ProcessMyEMSLDownloadQueue(m_WorkingDir, MyEMSLReader.Downloader.DownloadFolderLayout.FlatNoSubfolders) Then
-                        Return CloseOutType.CLOSEOUT_FAILED
-                    End If
-
-                    ' Confirm that the .Raw or .D folder was actually copied locally
-                    If strRawDataType.ToLower() = RAW_DATA_TYPE_DOT_RAW_FILES Then
-                        If Not File.Exists(Path.Combine(m_WorkingDir, DatasetName & DOT_RAW_EXTENSION)) Then
-                            m_message = "Thermo .Raw file not successfully copied to WorkDir; likely a timeout error"
-                            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "clsDtaGenResources.GetResources: " & m_message)
-                            Return CloseOutType.CLOSEOUT_FAILED
-                        End If
-                        m_jobParams.AddResultFileExtensionToSkip(DOT_RAW_EXTENSION)  'Raw file
-
-                    ElseIf strRawDataType.ToLower() = RAW_DATA_TYPE_BRUKER_FT_FOLDER Then
-                        If Not Directory.Exists(Path.Combine(m_WorkingDir, DatasetName & DOT_D_EXTENSION)) Then
-                            m_message = "Bruker .D folder not successfully copied to WorkDir; likely a timeout error"
-                            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "clsDtaGenResources.GetResources: " & m_message)
-                            Return CloseOutType.CLOSEOUT_FAILED
-                        End If
-                    End If
-
-                Else
-                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, "clsDtaGenResources.GetResources: Error occurred retrieving spectra.")
-                    Return CloseOutType.CLOSEOUT_FAILED
-                End If
-            Case Else
-                m_message = "Dataset type " & strRawDataType & " is not supported"
-                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, "clsDtaGenResources.GetResources: " & m_message & "; must be " & RAW_DATA_TYPE_DOT_RAW_FILES & " or " & RAW_DATA_TYPE_BRUKER_FT_FOLDER)
-                Return CloseOutType.CLOSEOUT_FAILED
-        End Select
-
-        If Not MyBase.ProcessMyEMSLDownloadQueue(m_WorkingDir, MyEMSLReader.Downloader.DownloadFolderLayout.FlatNoSubfolders) Then
-            Return CloseOutType.CLOSEOUT_FAILED
-        End If
-
-        Return CloseOutType.CLOSEOUT_SUCCESS
-
-    End Function
-
-
-End Class
+            return CloseOutType.CLOSEOUT_SUCCESS;
+        }
+    }
+}
