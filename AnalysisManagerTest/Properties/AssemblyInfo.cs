ecdb29fe4cbba09309df741a2fba554678900af0
alchemistmatt <matthew.monroe@pnnl.gov>
2017-05-17 20:45:41 -0700

    Add unit test project

---
+++ AnalysisManagerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalysisManagerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PNNL")]
+[assembly: AssemblyProduct("AnalysisManagerTest")]
+[assembly: AssemblyCopyright("Copyright © PNNL 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d143c403-faf2-4b3e-8603-1b37221a23fd")]
+
+[assembly: AssemblyVersion("1.0.*")]
