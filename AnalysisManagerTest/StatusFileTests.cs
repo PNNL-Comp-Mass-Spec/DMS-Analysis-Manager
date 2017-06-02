5fee7374e4e278e89c73b542565de4d61c4f00f2
alchemistmatt <matthew.monroe@pnnl.gov>
2017-06-02 11:35:38 -0700

    Add StatusFile test class

---
+++ AnalysisManagerTest/StatusFileTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using AnalysisManagerBase;
+using NUnit.Framework;
+using PRISM;
+
+namespace AnalysisManagerTest
+{
+    [TestFixture]
+    public class StatusFileTests
+    {
+        [Test]
+        public void TestWriteStatusFile()
+        {
+            var statusFile = new FileInfo("StatusTest.xml");
+            var debugLevel = 2;
+
+            var statusTools = new clsStatusFile(statusFile.FullName, debugLevel);
+            RegisterEvents(statusTools);
+
+            statusTools.UpdateAndWrite(EnumMgrStatus.STOPPED, EnumTaskStatus.STOPPED, EnumTaskStatusDetail.NO_TASK, 0);
+
+            Console.WriteLine("Status written to " + statusTools.FileNamePath);
+        }
+
+
+        #region "clsEventNotifier events"
+
+        private void RegisterEvents(clsEventNotifier oProcessingClass)
+        {
+            oProcessingClass.DebugEvent += DebugEventHandler;
+            oProcessingClass.StatusEvent += StatusEventHandler;
+            oProcessingClass.ErrorEvent += ErrorEventHandler;
+            oProcessingClass.WarningEvent += WarningEventHandler;
+        }
+
+        private void DebugEventHandler(string statusMessage)
+        {
+            Console.WriteLine(statusMessage);
+        }
+
+        private void StatusEventHandler(string statusMessage)
+        {
+            Console.WriteLine(statusMessage);
+        }
+
+        private void ErrorEventHandler(string errorMessage, Exception ex)
+        {
+            Console.WriteLine("Error: " + errorMessage);
+            Console.WriteLine(clsStackTraceFormatter.GetExceptionStackTraceMultiLine(ex));
+        }
+
+        private void WarningEventHandler(string warningMessage)
+        {
+            Console.WriteLine("Warning: " + warningMessage);
+        }
+
+        #endregion
+    }
+}
