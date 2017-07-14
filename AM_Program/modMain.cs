afa3b8cbce99b698eabc832960a8e44a6f736655
alchemistmatt <matthew.monroe@pnnl.gov>
2017-07-13 18:07:43 -0700

    Update the program date

--- AM_Program/modMain.cs
+++ AM_Program/modMain.cs
@@ -33,7 +33,7 @@ namespace AnalysisManagerProg
 {
     static class modMain
     {
-        public const string PROGRAM_DATE = "July 11, 2017";
+        public const string PROGRAM_DATE = "July 13, 2017";
 
         private static bool mCodeTestMode;
         private static bool mCreateWindowsEventLog;
