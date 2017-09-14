8d789232900567235ad96e14b73a4a980fa48144
alchemistmatt <matthew.monroe@pnnl.gov>
2017-09-13 19:23:29 -0700

    Tabs to spaces

--- Plugins/AM_MultiAlign_Aggregator_PlugIn/clsAnalysisResourcesMultiAlignAggregator.cs
+++ Plugins/AM_MultiAlign_Aggregator_PlugIn/clsAnalysisResourcesMultiAlignAggregator.cs
@@ -5,28 +5,28 @@ namespace AnalysisManagerMultiAlign_AggregatorPlugIn
     /// <summary>
     /// Retrieve resources for the MultiAlign Aggregator plugin
     /// </summary>
-	public class clsAnalysisResourcesMultiAlignAggregator : clsAnalysisResources
-	{
+    public class clsAnalysisResourcesMultiAlignAggregator : clsAnalysisResources
+    {
 
         /// <summary>
         /// Retrieve required files
         /// </summary>
         /// <returns>Closeout code</returns>
-		public override CloseOutType GetResources()
-		{
-			var SearchType = m_jobParams.GetParam("MultiAlignSearchType");
+        public override CloseOutType GetResources()
+        {
+            var SearchType = m_jobParams.GetParam("MultiAlignSearchType");
 
-			if (string.IsNullOrEmpty(SearchType))
-			{
-				m_message = "Parameter MultiAlignSearchType must be \"_LCMSFeatures.txt\" or \"_isos.csv\"";
-				return CloseOutType.CLOSEOUT_FILE_NOT_FOUND;
-			}
+            if (string.IsNullOrEmpty(SearchType))
+            {
+                m_message = "Parameter MultiAlignSearchType must be \"_LCMSFeatures.txt\" or \"_isos.csv\"";
+                return CloseOutType.CLOSEOUT_FILE_NOT_FOUND;
+            }
 
-			m_jobParams.AddResultFileExtensionToSkip(SearchType);
+            m_jobParams.AddResultFileExtensionToSkip(SearchType);
 
-			return CloseOutType.CLOSEOUT_SUCCESS;
+            return CloseOutType.CLOSEOUT_SUCCESS;
 
-		}
+        }
 
-	}
+    }
 }
