cd1a17e7286460d905eaf3ff9d850e478b57c05b
alchemistmatt <matthew.monroe@pnnl.gov>
2018-03-02 14:32:55 -0800

    Update DLLs and program date

--- AM_Program/modMain.cs
+++ AM_Program/modMain.cs
@@ -33,7 +33,7 @@ namespace AnalysisManagerProg
 {
     static class modMain
     {
-        public const string PROGRAM_DATE = "February 20, 2018";
+        public const string PROGRAM_DATE = "March 1, 2018";
 
         private static bool mCodeTestMode;
         private static bool mTraceMode;
