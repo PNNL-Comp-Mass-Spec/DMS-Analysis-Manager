cd2995f51880c27fbb675a6267895822495e821e
alchemistmatt <matthew.monroe@pnnl.gov>
2018-06-05 22:24:34 -0700

    Update the program date

--- AM_Program/modMain.cs
+++ AM_Program/modMain.cs
@@ -32,7 +32,7 @@ namespace AnalysisManagerProg
 {
     static class modMain
     {
-        public const string PROGRAM_DATE = "May 29, 2018";
+        public const string PROGRAM_DATE = "June 5, 2018";
 
         private static bool mCodeTestMode;
         private static bool mTraceMode;
