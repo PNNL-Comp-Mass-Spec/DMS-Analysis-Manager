5b1e74104dab0493cf93a275a5e3420116fd4c47
alchemistmatt <matthew.monroe@pnnl.gov>
2019-03-18 22:01:44 -0700

    Update the program date

--- AM_Program/Program.cs
+++ AM_Program/Program.cs
@@ -25,7 +25,7 @@ namespace AnalysisManagerProg
 {
     static class Program
     {
-        public const string PROGRAM_DATE = "March 5, 2019";
+        public const string PROGRAM_DATE = "March 18, 2019";
 
         private static bool mCodeTestMode;
         private static bool mTraceMode;
