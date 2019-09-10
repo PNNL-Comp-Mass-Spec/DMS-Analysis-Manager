1f4dc6b6539741b9fc8920c88ebd658e3bb171aa
alchemistmatt <matthew.monroe@pnnl.gov>
2019-09-10 16:40:38 -0700

    Update MyEMSLReader

--- AM_Program/Program.cs
+++ AM_Program/Program.cs
@@ -25,7 +25,7 @@ namespace AnalysisManagerProg
 {
     static class Program
     {
-        public const string PROGRAM_DATE = "August 1, 2019";
+        public const string PROGRAM_DATE = "September 3, 2019";
 
         private static bool mCodeTestMode;
         private static bool mTraceMode;
