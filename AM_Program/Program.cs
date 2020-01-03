871ddbe1a3143d8466e45e86244c7a6dfb786771
alchemistmatt <matthew.monroe@pnnl.gov>
2020-01-02 16:31:24 -0800

    Update packages from NuGet

--- AM_Program/Program.cs
+++ AM_Program/Program.cs
@@ -25,7 +25,7 @@ namespace AnalysisManagerProg
 {
     static class Program
     {
-        public const string PROGRAM_DATE = "October 18, 2019";
+        public const string PROGRAM_DATE = "January 2, 2020";
 
         private static bool mCodeTestMode;
         private static bool mTraceMode;
