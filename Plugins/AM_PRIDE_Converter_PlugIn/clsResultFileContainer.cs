26aff7fce57cae4ffda76634c02dbde3c1409747
alchemistmatt <matthew.monroe@pnnl.gov>
2021-01-01 16:00:31 -0800

    Remove empty comments

--- Plugins/AM_PRIDE_Converter_PlugIn/clsResultFileContainer.cs
+++ Plugins/AM_PRIDE_Converter_PlugIn/clsResultFileContainer.cs
@@ -10,25 +10,16 @@ namespace AnalysisManagerPRIDEConverterPlugIn
         /// <summary>
         /// Tracks the .mgf or _dta.txt or .mzML file for the analysis job
         /// </summary>
-        /// <value></value>
-        /// <returns></returns>
-        /// <remarks></remarks>
         public string MGFFilePath { get; set; }
 
         /// <summary>
         /// One or more .mzid.gz files
         /// </summary>
-        /// <value></value>
-        /// <returns></returns>
-        /// <remarks></remarks>
         public List<string> MzIDFilePaths { get; set; }
 
         /// <summary>
         /// Tracks the .pepXML.gz file for the analysis job
         /// </summary>
-        /// <value></value>
-        /// <returns></returns>
-        /// <remarks></remarks>
         public string PepXMLFile { get; set; }
 
         /// <summary>
@@ -39,7 +30,6 @@ namespace AnalysisManagerPRIDEConverterPlugIn
         /// <summary>
         /// Constructor
         /// </summary>
-        /// <remarks></remarks>
         public clsResultFileContainer()
         {
             MzIDFilePaths = new List<string>();
