bfe88bcf21c7aca5c514cc1620a956445d0435fa
alchemistmatt <matthew.monroe@pnnl.gov>
2021-03-04 17:55:01 -0800

    Suppress warnings

--- Plugins/AM_DTASpectraFileGen_PlugIn/GlobalSuppressions.cs
+++ Plugins/AM_DTASpectraFileGen_PlugIn/GlobalSuppressions.cs
@@ -6,13 +6,12 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGenThermoRaw")]
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGenDeconConsole")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Ignore errors here", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenThermoRaw.UpdateDeconMSnProgress(System.String)")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Ignore errors here", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenThermoRaw.UpdateDTAProgress(System.String)")]
 [assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenDeconConsole.ParseDeconToolsLogFile(System.Boolean@,System.DateTime@)")]
 [assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenThermoRaw.MakeDTAFiles~System.Boolean")]
 [assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenToolRunner.StartAndWaitForDTAGenerator(AnalysisManagerBase.ISpectraFileProcessor,System.String,System.Boolean)~AnalysisManagerBase.CloseOutType")]
 [assembly: SuppressMessage("Style", "IDE0066:Convert switch statement to expression", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenDeconConsole.ParseDeconToolsLogFile(System.Boolean@,System.DateTime@)")]
-[assembly: SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenToolRunner.RemoveTitleAndParentIonLines(System.String)~System.String")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGen")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGenMSConvert")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGenRawConverter")]
@@ -20,3 +19,6 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGenToolRunner")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:DTASpectraFileGen.MGFConverter")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:DTASpectraFileGen.MGFtoDtaGenMainProcess")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGenDeconConsole")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:DTASpectraFileGen.DtaGenThermoRaw")]
+[assembly: SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:DTASpectraFileGen.DtaGenToolRunner.RemoveTitleAndParentIonLines(System.String)~System.String")]
