f71c0490b88fe9b33e6a1754ca2e90777ade621a
alchemistmatt <matthew.monroe@pnnl.gov>
2021-03-08 13:19:32 -0800

    Suppress warnings

--- Plugins/AM_Extraction_PlugIn/GlobalSuppressions.cs
+++ Plugins/AM_Extraction_PlugIn/GlobalSuppressions.cs
@@ -5,8 +5,10 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:AnalysisManagerExtractionPlugin.ExtractToolRunner.PHRP_ProgressChanged(System.String,System.Single)")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Ignore errors here", Scope = "member", Target = "~M:AnalysisManagerExtractionPlugin.ExtractToolRunner.RunPhrpForInSpecT~AnalysisManagerBase.CloseOutType")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Ignore errors here", Scope = "member", Target = "~M:AnalysisManagerExtractionPlugin.ExtractToolRunner.RunPhrpForMSGFPlus~AnalysisManagerBase.CloseOutType")]
 [assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Ignore errors deleting this file", Scope = "member", Target = "~M:AnalysisManagerExtractionPlugin.PepHitResultsProcWrapper.ExtractDataFromResults(System.String,System.Boolean,System.Boolean,System.String,System.String)~AnalysisManagerBase.CloseOutType")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:AnalysisManagerExtractionPlugin.ExtractToolRunner.PHRP_ProgressChanged(System.String,System.Single)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:AnalysisManagerExtractionPlugin.PepHitResultsProcWrapper")]
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:AnalysisManagerExtractionPlugin.PepHitResultsProcWrapper.ExtractDataFromResults(System.String,System.Boolean,System.Boolean,System.String,System.String)~AnalysisManagerBase.CloseOutType")]
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:AnalysisManagerExtractionPlugin.PepHitResultsProcWrapper.ValidatePrimaryResultsFile(System.IO.FileInfo,System.String,System.String)~AnalysisManagerBase.CloseOutType")]
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:AnalysisManagerExtractionPlugin.PepHitResultsProcWrapper")]
