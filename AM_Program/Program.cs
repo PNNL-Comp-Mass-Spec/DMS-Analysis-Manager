0d48eb82d9771bce292ff29ee02d35ef2adc7a36
alchemistmatt <matthew.monroe@pnnl.gov>
2021-04-05 17:59:47 -0700

    Add dictionary word and update program date

--- AM_Program/Program.cs
+++ AM_Program/Program.cs
@@ -30,7 +30,7 @@ namespace AnalysisManagerProg
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "March 26, 2021";
+        public const string PROGRAM_DATE = "April 5, 2021";
 
         private static bool mTraceMode;
 
