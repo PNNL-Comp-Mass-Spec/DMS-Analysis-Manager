6957891e78f4791d04268ec92271e61ec9a24827
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-07 17:59:42 -0700

    Adjust suppressed warning

--- Plugins/AM_MSGF_PlugIn/MSGFResultsSummarizerDLL/GlobalSuppressions.cs
+++ Plugins/AM_MSGF_PlugIn/MSGFResultsSummarizerDLL/GlobalSuppressions.cs
@@ -5,7 +5,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:MSGFResultsSummarizer.ResultsSummarizer.CheckForScanGaps(System.Collections.Generic.Dictionary{System.String,System.Collections.Generic.SortedSet{System.Int32}})")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:MSGFResultsSummarizer.ResultsSummarizer.CheckForScanGaps(System.Collections.Generic.Dictionary{System.String,System.Collections.Generic.List{System.Int32}})")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:MSGFResultsSummarizer.ResultsSummarizer")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:MSGFResultsSummarizer.ResultsSummarizer.MSGFtoResultIDMapComparer")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:MSGFResultsSummarizer.PSMStats")]
