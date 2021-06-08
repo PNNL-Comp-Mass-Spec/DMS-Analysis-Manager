7fd55aeffea5c97aeac2be79cc4d5feb8394aefc
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-07 17:59:30 -0700

    Remove method that is now unused since MaxQuant q-values are available

--- Plugins/AM_MSGF_PlugIn/MSGFResultsSummarizerDLL/ResultsSummarizer.cs
+++ Plugins/AM_MSGF_PlugIn/MSGFResultsSummarizerDLL/ResultsSummarizer.cs
@@ -304,32 +304,6 @@ namespace MSGFResultsSummarizer
             }
         }
 
-        /// <summary>
-        /// Copies peptide counts from mMSGFBasedCounts mFDRBasedCounts
-        /// This is used for MaxQuant data, since we don't have FDRs but we want to keep track of tryptic counts, phosphopeptide counts, etc.
-        /// </summary>
-        /// <remarks>
-        /// Does not copy the following values:
-        ///   TotalPSMs
-        ///   UniquePeptideCount
-        ///   UniqueProteinCount
-        /// </remarks>
-        public void CopyMSGFCountsToFDRCounts()
-        {
-            mFDRBasedCounts ??= new PSMStats();
-
-            mFDRBasedCounts.TrypticPeptides = mMSGFBasedCounts.TrypticPeptides;
-
-            mFDRBasedCounts.UniquePhosphopeptideCount = mMSGFBasedCounts.UniquePhosphopeptideCount;
-            mFDRBasedCounts.UniquePhosphopeptidesCTermK = mMSGFBasedCounts.UniquePhosphopeptidesCTermK;
-            mFDRBasedCounts.UniquePhosphopeptidesCTermR = mMSGFBasedCounts.UniquePhosphopeptidesCTermR;
-
-            mFDRBasedCounts.MissedCleavageRatio = mMSGFBasedCounts.MissedCleavageRatio;
-            mFDRBasedCounts.MissedCleavageRatioPhospho = mMSGFBasedCounts.MissedCleavageRatioPhospho;
-            mFDRBasedCounts.KeratinPeptides = mMSGFBasedCounts.KeratinPeptides;
-            mFDRBasedCounts.TrypsinPeptides = mMSGFBasedCounts.TrypsinPeptides;
-        }
-
         private void ExamineFirstHitsFile(string firstHitsFilePath)
         {
             try
@@ -1261,11 +1235,6 @@ namespace MSGFResultsSummarizer
 
                 if (!(success || successViaFDR))
                     return false;
-
-                if (ResultType == PeptideHitResultTypes.MaxQuant && mFDRBasedCounts == null)
-                {
-                    CopyMSGFCountsToFDRCounts();
-                }
 
                 if (SaveResultsToTextFile)
                 {
