79d92a731ab9e40d9d1959a7a3411f5dc5bd4d0b
alchemistmatt <matthew.monroe@pnnl.gov>
2021-07-14 18:40:23 -0700

    Update the MSFragger resourcer to support data package-based jobs

--- Plugins/AM_MSFragger_Plugin/AnalysisResourcesMSFragger.cs
+++ Plugins/AM_MSFragger_Plugin/AnalysisResourcesMSFragger.cs
@@ -6,8 +6,10 @@
 //*********************************************************************************************************
 
 using System;
+using System.IO;
 using AnalysisManagerBase;
 using AnalysisManagerBase.AnalysisTool;
+using AnalysisManagerBase.DataFileTools;
 using AnalysisManagerBase.JobConfig;
 using AnalysisManagerBase.StatusReporting;
 
@@ -18,7 +20,7 @@ namespace AnalysisManagerMSFraggerPlugIn
     /// </summary>
     public class AnalysisResourcesMSFragger : AnalysisResources
     {
-        // Ignore Spelling: Fragger, ParmFile
+        // Ignore Spelling: Fragger, ParmFile, resourcer
 
         /// <summary>
         /// Initialize options
@@ -55,6 +57,29 @@ namespace AnalysisManagerMSFraggerPlugIn
                     return CloseOutType.CLOSEOUT_FAILED;
                 }
 
+                var workingDirectory = new DirectoryInfo(mWorkDir);
+
+                var dataPackageID = mJobParams.GetJobParameter("DataPackageID", 0);
+
+                // Require that the input files be mzML files (since PeptideProphet prefers them and TmtIntegrator requires them)
+                // In contrast, MaxQuant can work with either .raw files or .mzML files
+                const bool usingMzML = true;
+
+                // Determine the transfer directory path
+
+                // Caveats for usingMzML:
+                //   When true, we want useInputDirectory to be true so that the resourcer will look for .mzML files in a MSXML_Gen directory
+                //   When usingMzML is false, we want useInputDirectory to be false so that the resourcer will look for .Raw files in the dataset directory
+
+                // Caveats for dataPackageID
+                //   When 0, we are processing a single dataset, and we thus need to include the dataset name, generating a path like \\proto-4\DMS3_Xfer\QC_Dataset\MXQ202103151122_Auto1880613
+                //   When positive, we are processing datasets in a data package, and we thus want a path without the dataset name, generating a path like \\proto-9\MaxQuant_Staging\MXQ202103161252_Auto1880833
+
+                var useInputDirectory = usingMzML;
+                var includeDatasetName = dataPackageID <= 0;
+
+                var transferDirectoryPath = GetTransferDirectoryPathForJobStep(useInputDirectory, includeDatasetName);
+
                 var paramFileName = mJobParams.GetParam(JOB_PARAM_PARAMETER_FILE);
                 currentTask = "RetrieveParamFile " + paramFileName;
 
@@ -69,14 +94,29 @@ namespace AnalysisManagerMSFraggerPlugIn
                 if (!RetrieveOrgDB(orgDbDirectoryPath, out var resultCode))
                     return resultCode;
 
-                currentTask = "GetMzMLFile";
+                var datasetFileRetriever = new DatasetFileRetriever(this);
+                RegisterEvents(datasetFileRetriever);
 
-                var mzMLResultCode = GetMzMLFile();
+                var datasetCopyResult = datasetFileRetriever.RetrieveInstrumentFilesForJobDatasets(
+                    dataPackageID,
+                    usingMzML,
+                    AnalysisToolRunnerMSFragger.PROGRESS_PCT_STARTING,
+                    out var dataPackageInfo,
+                    out _);
 
-                if (mzMLResultCode != CloseOutType.CLOSEOUT_SUCCESS)
+                if (datasetCopyResult != CloseOutType.CLOSEOUT_SUCCESS)
                 {
-                    return mzMLResultCode;
+                    if (!string.IsNullOrWhiteSpace(datasetFileRetriever.ErrorMessage))
+                    {
+                        mMessage = datasetFileRetriever.ErrorMessage;
+                    }
+
+                    return datasetCopyResult;
                 }
+
+                // Store information about the datasets in several packed job parameters
+                dataPackageInfo.StorePackedDictionaries(this);
+
 
                 if (!ProcessMyEMSLDownloadQueue(mWorkDir, MyEMSLReader.Downloader.DownloadLayout.FlatNoSubdirectories))
                 {
@@ -92,4 +132,5 @@ namespace AnalysisManagerMSFraggerPlugIn
             }
         }
     }
+
 }
