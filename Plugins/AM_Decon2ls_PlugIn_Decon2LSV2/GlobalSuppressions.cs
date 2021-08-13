b844f0735ec17f78ec666f824bc487cd887fc400
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-12 18:00:18 -0700

    Update suppressed warnings

--- Plugins/AM_Decon2ls_PlugIn_Decon2LSV2/GlobalSuppressions.cs
+++ Plugins/AM_Decon2ls_PlugIn_Decon2LSV2/GlobalSuppressions.cs
@@ -11,7 +11,6 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE0066:Convert switch statement to expression", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:AnalysisManagerDecon2lsV2PlugIn.AnalysisToolRunnerDecon2ls.ParseDeconToolsLogFile(System.Boolean@,System.DateTime@)")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:AnalysisManagerDecon2lsV2PlugIn.AnalysisResourcesDecon2ls")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:AnalysisManagerDecon2lsV2PlugIn.AnalysisToolRunnerDecon2ls")]
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:AnalysisManagerDecon2lsV2PlugIn.AnalysisToolRunnerDecon2ls.udtDeconToolsStatusType")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:AnalysisManagerDecon2lsV2PlugIn.DeconToolsQCPlotsGenerator")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:AnalysisManagerDecon2lsV2PlugIn.XMLParamFileReader")]
 [assembly: SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:AnalysisManagerDecon2lsV2PlugIn.AnalysisResourcesDecon2ls.IsMSMSProcessingEnabled(System.IO.FileSystemInfo,System.Boolean@)~System.Boolean")]
--- Plugins/AM_OMSSA_PlugIn/GlobalSuppressions.cs
+++ Plugins/AM_OMSSA_PlugIn/GlobalSuppressions.cs
@@ -5,8 +5,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Leave for reference", Scope = "member", Target = "~F:AnalysisManagerOMSSAPlugIn.AnalysisToolRunnerOM.PROGRESS_PCT_PEPTIDEHIT_COMPLETE")]
-[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Leave for reference", Scope = "member", Target = "~F:AnalysisManagerOMSSAPlugIn.AnalysisToolRunnerOM.PROGRESS_PCT_PEPTIDEHIT_START")]
-[assembly: SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "Leave for reference", Scope = "member", Target = "~F:AnalysisManagerOMSSAPlugIn.AnalysisToolRunnerOM.PROGRESS_PCT_PEPTIDEHIT_COMPLETE")]
-[assembly: SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "Leave for reference", Scope = "member", Target = "~F:AnalysisManagerOMSSAPlugIn.AnalysisToolRunnerOM.PROGRESS_PCT_PEPTIDEHIT_START")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable legacy name", Scope = "type", Target = "~T:AnalysisManagerOMSSAPlugIn.AnalysisToolRunnerOM")]
