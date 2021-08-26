1da5927835bb90811761b4b7abc04a68a3c2e7d5
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-25 18:03:45 -0700

    Flag method as obsolete

--- Plugins/AM_Sequest_PlugIn/AnalysisToolRunnerAgilentSeq.cs
+++ Plugins/AM_Sequest_PlugIn/AnalysisToolRunnerAgilentSeq.cs
@@ -8,6 +8,7 @@ namespace AnalysisManagerSequestPlugin
     {
         // Ignore Spelling: cdf, mgf
 
+        [Obsolete("Unused")]
         private CloseOutType DeleteDataFile()
         {
             // Deletes the data files (.mgf and .cdf) from the working directory
