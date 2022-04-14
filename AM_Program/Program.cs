ff746be8ae0991ecb9dd49b37c64dd4fce6a7357
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-13 20:06:48 -0700

    New version of PRISM.dll that moves zipped log files into an archive directory

--- AM_Program/Program.cs
+++ AM_Program/Program.cs
@@ -30,7 +30,7 @@ namespace AnalysisManagerProg
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "April 6, 2022";
+        public const string PROGRAM_DATE = "April 13, 2022";
 
         private static bool mTraceMode;
 
