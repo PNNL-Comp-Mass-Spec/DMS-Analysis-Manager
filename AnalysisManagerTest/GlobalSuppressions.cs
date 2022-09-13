c0edcd9ccc9609b040ebdf5dfd85a4dcf6a08c76
alchemistmatt <matthew.monroe@pnnl.gov>
2022-09-13 13:08:15 -0700

    Suppress warning

---
+++ AnalysisManagerTest/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Roslynator", "RCS1225:Make class sealed.", Justification = "Leave as-is", Scope = "type", Target = "~T:AnalysisManagerTest.StatusFileTests")]
