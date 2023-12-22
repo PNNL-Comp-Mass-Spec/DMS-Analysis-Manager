629a0f9e5f99f9dc77fa3934a92b0b8bc36dd0a3
alchemistmatt <matthew.monroe@pnnl.gov>
2023-12-21 16:43:17 -0800

    Suppress warnings

--- Plugins/AM_MSFragger_Plugin/GlobalSuppressions.cs
+++ Plugins/AM_MSFragger_Plugin/GlobalSuppressions.cs
@@ -6,5 +6,6 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:AnalysisManagerMSFraggerPlugIn.AnalysisResourcesMSFragger.GetJavaMemorySizeToUse(AnalysisManagerBase.JobConfig.IJobParams,System.Double,System.Int32@)~System.Int32")]
+[assembly: SuppressMessage("Roslynator", "RCS1123:Add parentheses when necessary", Justification = "Parentheses not needed", Scope = "member", Target = "~M:AnalysisManagerMSFraggerPlugIn.AnalysisResourcesMSFragger.GetJavaMemorySizeToUse(AnalysisManagerBase.JobConfig.IJobParams,System.Double,System.Int32,System.Int32@)~System.Int32")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:AnalysisManagerMSFraggerPlugIn.AnalysisResourcesMSFragger")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed", Scope = "type", Target = "~T:AnalysisManagerMSFraggerPlugIn.AnalysisToolRunnerMSFragger")]
